4c5fafaa00fc0a1f10126140e332f8b7d4952c06
Sean Hamilton <sean@hamistudios.com>
2018-11-29 16:43:46 +0000

    feat(index): export all new function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
+// methods
+import clone from './clone';
+import empty from './empty';
+import get from './get';
+import has from './has';
+import is from './is';
+import keys from './keys';
+import size from './size';
 
+export {
+  clone,
+  empty,
+  get,
+  has,
+  is,
+  keys,
+  size,
+};
+
+/*
+  All methods in alphabetical order
+
+  clean
+  clone
+  each
+  empty
+  every
+  filter
+  find
+  flip
+  get
+  has
+  includes
+  is
+  keyOf
+  keys
+  map
+  merge
+  prepend
+  set
+  size
+  slice
+  some
+  sort
+  values
+ */
