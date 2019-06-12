a40f38e017a06f75f8c4a8b2c01a856ab61b464d
Sean Hamilton <sean@hamistudios.com>
2019-06-12 14:51:14 +0100

    build(rollup): add min bundle support

--- rollup.config.js
+++ rollup.config.js
@@ -2,29 +2,53 @@
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
 
 // package.json
 import pkg from './package.json';
 
+const input = './src/index.ts';
 const extensions = ['.js', '.ts'];
+const plugins = [
+  resolve({
+    extensions,
+  }),
+  commonjs(),
+  babel({
+    extensions,
+    include: ['src/**/*'],
+  }),
+];
+const format = 'umd';
+const banner = `/* o - v${pkg.version}\n *\n * Released under MIT license\n * https://github.com/hammy2899/o\n */\n`;
+const name = 'o';
 
-export default {
-  input: './src/index.ts',
-  plugins: [
-    resolve({
-      extensions,
-    }),
-    commonjs(),
-    babel({
-      extensions,
-      include: ['src/**/*'],
-    }),
-  ],
-  output: {
-    file: 'dist/o.js',
-    format: 'umd',
-    sourceMap: true,
-    banner: `/* o - v${pkg.version}\n *\n * Released under MIT license\n * https://github.com/hammy2899/o\n */\n`,
-    name: 'o',
+export default [
+  {
+    input,
+    plugins,
+    output: {
+      file: 'dist/o.js',
+      format,
+      sourcemap: true,
+      banner,
+      name,
+    },
   },
-};
+  {
+    input,
+    plugins: [
+      ...plugins,
+      terser({
+        sourcemap: true,
+      }),
+    ],
+    output: {
+      file: 'dist/o.min.js',
+      format,
+      sourcemap: true,
+      banner,
+      name,
+    },
+  },
+];
