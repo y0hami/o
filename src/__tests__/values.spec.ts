643c0eacbf4bf4593c8e8d93ce78487488fd889b
Sean Hamilton <sean@hamistudios.com>
2019-07-04 11:41:42 +0100

    test(values): add values function tests

---
+++ src/__tests__/values.spec.ts
@@ -0,0 +1,51 @@
+import values from '../values';
+
+describe('values', () => {
+  test('should return an array of the objects values', () => {
+    const obj = {
+      a: 1,
+      b: 2,
+      c: 3,
+    };
+
+    const result = values(obj);
+
+    expect(result).toHaveLength(3);
+    expect(result[0]).toBe(1);
+    expect(result[1]).toBe(2);
+    expect(result[2]).toBe(3);
+  });
+
+  test('should return values of deep objects when follow is true', () => {
+    const obj = {
+      a: 1,
+      b: 2,
+      c: {
+        d: 3,
+        e: 4,
+      },
+    };
+
+    const result = values(obj, {
+      follow: true,
+    });
+
+    expect(result).toHaveLength(4);
+    expect(result[0]).toBe(1);
+    expect(result[1]).toBe(2);
+    expect(result[2]).toBe(3);
+    expect(result[3]).toBe(4);
+  });
+
+  test('should throw TypeError for invalid arguments', () => {
+    const invalidObj: unknown = 'testing';
+    const invalidFollow: unknown = 'testing';
+
+    expect(() => values(invalidObj as OObject))
+      .toThrow(new TypeError('Expected Object, got string testing'));
+    expect(() => values({}, {
+      follow: invalidFollow as boolean,
+    }))
+      .toThrow(new TypeError('Expected Boolean, got string testing'));
+  });
+});
