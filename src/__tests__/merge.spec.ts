47d9a950f76d37b948c9f941ab8b58f85791889f
Sean Hamilton <sean@hamistudios.com>
2019-07-04 10:14:44 +0100

    test(merge): add merge function tests

---
+++ src/__tests__/merge.spec.ts
@@ -0,0 +1,46 @@
+import merge from '../merge';
+
+describe('merge', () => {
+  test('should return a new object not a reference', () => {
+    const obj = {
+      a: 1,
+      b: 2,
+    };
+
+    const result = merge(obj, {});
+    result.a = 2;
+
+    expect(obj.a).toBe(1);
+  });
+
+  test('should return an object with the properties of the sources merged on top', () => {
+    const target = {
+      a: 1,
+      b: 2,
+    };
+    const sourceA = {
+      a: 2,
+    };
+    const sourceB = {
+      b: 3,
+    };
+
+    const resultA = merge(target, sourceA);
+    const resultB = merge(target, sourceA, sourceB);
+
+    expect(resultA.a).toBe(2);
+    expect(resultB.a).toBe(2);
+    expect(resultB.b).toBe(3);
+  });
+
+  test('', () => {
+    const invalidObj: unknown = 'testing';
+
+    expect(() => merge(invalidObj as OObject, {}))
+      .toThrow(new TypeError('Expected Object, got string testing'));
+    expect(() => merge({}, invalidObj as OObject))
+      .toThrow(new TypeError('Expected Object[], got object testing'));
+    expect(() => merge({}, {}, invalidObj as OObject))
+      .toThrow(new TypeError('Expected Object[], got object [object Object],testing'));
+  });
+});
