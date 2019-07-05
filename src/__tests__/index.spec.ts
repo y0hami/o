3db43e7b74c1ed79c4aaa244b7b0d258ea91b912
Sean Hamilton <sean@hamistudios.com>
2019-07-05 09:04:36 +0100

    test(index): fix tests

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,12 +1,15 @@
 import * as o from '../index';
 
 describe('index', () => {
-  test('should have 27 functions', () => {
-    expect(Object.keys(o)).toHaveLength(27);
+  test('should have 27 functions (+ default)', () => {
+    expect(Object.keys(o)).toHaveLength(28);
   });
 
   test('all key values should be functions', () => {
-    expect(Object.values(o)
+    const funcs = Object.assign({}, o);
+    delete funcs.default;
+
+    expect(Object.values(funcs)
       .every(v => typeof v === 'function')).toBe(true);
   });
 });
